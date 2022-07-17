8fdb07d6c5a27cdd35fa042a39a4ab04e3ba9069
Nazrin.Putra <nazrin.putra@tm.com.my>
2022-07-17 16:03:32 +0800

    Configure heroku and cleardb

--- db.php
+++ db.php
@@ -16,9 +16,16 @@ if (isset($_SESSION["id"])) {
     $logged_in = false;
 }
 
-$hostname = "localhost";
-$username = "root";
-$password = "Passw0rd";
-$dbname = "anas";
+if (getenv('ENVIRONMENT' == 'PROD')) {
+    $hostname = "us-cdbr-east-06.cleardb.net";
+    $username = "bd7154bcfb7751";
+    $password = "41b3457f";
+    $dbname = "heroku_fe00c8042680717";
+} else {
+    $hostname = "localhost";
+    $username = "root";
+    $password = "Passw0rd";
+    $dbname = "anas";
+}
 
 $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.");
